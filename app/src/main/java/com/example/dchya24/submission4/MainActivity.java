e67f477857e18f67f6ffd8f7308f134d07b31e24
dchya24 <cahyadinar241@gmail.com>
2019-07-22 17:00:50 +0700

    edited bottom navigator

--- app/src/main/java/com/example/dchya24/submission4/MainActivity.java
+++ app/src/main/java/com/example/dchya24/submission4/MainActivity.java
@@ -17,14 +17,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_movie:
+                    mTextMessage.setText(R.string.navigation_movie);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_tv_show:
+                    mTextMessage.setText(R.string.navigation_tv_show);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_movie_favorit:
+                    mTextMessage.setText(R.string.navigation_movie_favorit);
+                    return true;
+                case R.id.navigation_tv_show_favorit:
+                    mTextMessage.setText(R.string.navigation_tv_show_favorit);
                     return true;
             }
             return false;
@@ -36,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
